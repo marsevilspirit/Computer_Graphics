9dcda36e44897ded10a5bc80a6e078fed155a0fa
marsevilspirit <a2261214089@qq.com>
2024-04-05 13:25:12 +0800

    finish(pan 1) nice

--- pa1/main.cpp
+++ pa1/main.cpp
@@ -27,6 +27,17 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
 
+    double angle = rotation_angle / 180.0 * MY_PI;//转化成弧度
+
+    Eigen::Matrix4f r;
+
+    r << std::cos(angle), -std::sin(angle), 0, 0,
+         std::sin(angle), std::cos(angle), 0, 0,
+         0, 0, 1, 0,
+         0, 0, 0, 1;
+
+    model = model * r;
+
     return model;
 }
 
@@ -40,6 +51,44 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+
+    float angel = eye_fov / 180.0 * MY_PI;
+    float t = zNear * std::tan(angel/2);
+    float r = t * aspect_ratio;
+    float l = -r;
+    float b = -t;
+
+    Eigen::Matrix4f MorthoScale(4,4);
+    MorthoScale << 2/(r - l) , 0, 0, 0,
+            0, 2/(t - b) , 0, 0,
+            0, 0, 2/(zFar - zNear), 0,
+            0, 0, 0, 1;
+
+    Eigen::Matrix4f MorthoPos(4,4);
+    MorthoPos << 1, 0, 0, -(r + l)/2,
+            0, 1, 0, -(t + b)/2,
+            0, 0, 1, -(zNear + zFar)/2,
+            0, 0, 0, 1;
+    
+
+    Eigen::Matrix4f Mpersp2ortho(4,4);
+
+    Mpersp2ortho << zNear, 0, 0, 0,
+                0, zNear, 0, 0,
+                0, 0, zNear + zFar, -zNear * zFar,
+                0, 0, 1, 0;
+
+    //为了使得三角形是正着显示的，这里需要把透视矩阵乘以下面这样的矩阵
+    //参考：http://games-cn.org/forums/topic/%e4%bd%9c%e4%b8%9a%e4%b8%89%e7%9a%84%e7%89%9b%e5%80%92%e8%bf%87%e6%9d%a5%e4%ba%86/
+    Eigen::Matrix4f Mt(4,4);
+    Mt << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, -1, 0,
+        0, 0, 0, 1; 
+    Mpersp2ortho = Mpersp2ortho *Mt;
+
+    projection = MorthoScale * MorthoPos * Mpersp2ortho * projection;
+
 
     return projection;
 }
